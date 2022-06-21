71946a1a2f4532269d940d3d27f3e5ed148969d7
Naldo Assis <ronaldoassisj@gmail.com>
2022-06-21 18:25:05 -0300

    novos arquivos

---
+++ S4/s4-09/script.js
@@ -0,0 +1,34 @@
+// let nomeDosCompetidores = ["Maria", "Heric", "Roberta", "Rafael", "Lucas"]
+// let posicoesDoPodio = [1,2,3,4,5]
+
+// n = 0
+
+//     while (posicoesDoPodio[n] <= posicoesDoPodio.length) {
+//             console.log('Posição ' + posicoesDoPodio[n] + ' -> ' + nomeDosCompetidores[n])
+//             n++
+//     }
+
+    let quemNasceuPrimeiro = ["pai", "mae", "eu", "nan", "bento"]
+    let ordemDeNascimento = [1, 2, 3, 4, 5]
+    n = 0
+        while (ordemDeNascimento[n] <= ordemDeNascimento.length) {
+            console.log(ordemDeNascimento[n] + ' - ' + quemNasceuPrimeiro[n])
+            n++
+        }
+ 
+
+
+
+
+
+
+
+
+
+ // let ganhador = nomeDosCompetidores[0]
+// // console.log(ganhador);
+// // --------------------------------------
+
+// let incentivo = nomeDosCompetidores[nomeDosCompetidores.length - 1]
+//  // console.log(incentivo);
+// ------------------------------------------
