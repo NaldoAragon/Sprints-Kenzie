757add4f76b2af555503a11ed306ddbeb1d047b8
Naldo Assis <ronaldoassisj@gmail.com>
2022-06-25 21:09:02 -0300

    arquivo novo

--- lixo.js
+++ lixo.js
@@ -1,10 +1,124 @@
-let familia = ['pai', 'mae', 'eu', 'nan', 'bento']
+function somarNumeros(n1, n2, n3, n4=0){
+    return n1 + n2 + n3 + n4
+    // console.log(somatotal)
+}
 
-console.log(familia[0]);
+let result1 = somarNumeros(2, 3, 4)
+let result2 = somarNumeros(5,7,9)
+let resultFam = somarNumeros(37, 28, 62, 64)
 
-let amor = 0
-let marito = 'Chris'
-let novaFamilia = ['eu', marido, 'filho1', 'filho2']
+// console.log(result1);
+// console.log(result2); 
+alert(`A soma da idade da família é ${resultFam}`)
 
-let array = ['eu', amor, 'bento', 'família0', novaFamilia]
+function digaOla(){
+    return 'Olá!'
+}
+alert(digaOla())
 
+// let listaDeNomes = []
+
+// listaDeNomes.push(['naldinho', 'Ronaldo', 'naldoca', 'naldo', 'ronaldinho'])
+
+// console.log(listaDeNomes);
+
+//     function ordemNomes(listaNomes){
+
+//         for(i=0; i < listaNomes.length; i++){
+            
+//             if (listaNomes[i] == 'Ronaldo')
+
+//             return listaNomes[i]
+//         }
+//     }
+// let nomeEncontrado = ordemNomes(listaDeNomes);
+// console.log(nomeEncontrado);
+
+
+// function numeroNegativo (array){
+    
+//     for (i in array){
+        
+//         if(array[i] < 0){
+//             console.log(`Aqui estão só os números negativos ${array[i]}`);
+//         }
+//     }
+// }
+// numeroNegativo([10, 2, -4, -90, -50, 1, -10])
+
+
+// function parimp (n){
+
+//     if (n%2 == 0) {
+//         return 'par'
+//     }
+//     else {
+//         return 'impar'
+//     }
+// }
+// parimp(50)
+
+// function soma(n1, n2){
+
+//     return n1 + n2
+// }
+// soma(37, 41)
+
+
+// let array = [10, 2, -4, -90, -50, 1, -10]
+
+//     // for(i = 0; i < array.length; i++){
+
+//         for(i in array){
+
+//         console.log(`A posição ${i}, tem valor ${array[i]}`);
+//     }
+
+
+// function numeroNegativo(num){
+
+//     console.log(num);
+
+//     for(i=0 ; i  < numeroNegativo.length ; i++)
+
+// }
+
+// numeroNegativo([10, 2, -4, -90, -50, 1, -10])
+
+
+
+// let nome = prompt('Digite seu nome')
+
+// // console.table(nome)
+
+// let sobrenome = prompt('Digite seu sobrenome')
+// // console.table (sobrenome)
+
+// console.table(nome + ' ' + sobrenome)
+
+
+
+// let cont = 0
+
+
+    // let marido = 'Chris'
+    // let novaFamilia = ['eu', marido, 'filho1', 'filho2']
+    // amor = 'Deus'
+    // novaFamilia[1] = 'Deus'
+    // array = ['eu', amor, 'bento', 4, [novaFamilia]]
+
+// let mercado = []
+// mercado.push('chocolate')
+// mercado.push('carneMoida')
+// mercado.push('feijao')
+// mercado.push('arroz')
+// mercado.push('salada')
+
+// console.table(mercado);
+
+// console.table(array);
+    // for(cont = 0; cont < array.length; cont++){
+
+    // console.table('Quem vai conviver comigo ' + array[cont]);
+    // }
+    // console.log(typeof(array[3]));
